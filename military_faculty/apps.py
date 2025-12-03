1184d065634d8621bf4712feba7f560a1c892d5c
aktan228 <aktam200e@gmail.com>
2025-12-04 01:09:46 +0600

    ive initialized a new apps

---
+++ military_faculty/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MilitaryFacultyConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "military_faculty"
---
+++ pedagogical_faculty/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class PedagogicalFacultyConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "pedagogical_faculty"
